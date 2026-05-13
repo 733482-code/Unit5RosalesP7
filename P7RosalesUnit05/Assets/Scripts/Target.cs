20921ced505c8cd2210014a7ca87853ab4346929
733482-code <733482@student.dusd.net>
2026-05-13 15:17:36 -0700

    5.3
    working on 5.3

--- P7RosalesUnit05/Assets/Scripts/Target.cs
+++ P7RosalesUnit05/Assets/Scripts/Target.cs
@@ -10,6 +10,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    public ParticleSystem explosionParticle;
     public int pointValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,13 +33,21 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
